530c26359881f9b1e5c398f2efb8cd438f5c2074
Cleps <clecio.lord@gmail.com>
2024-05-22 17:40:51 -0500

    feat: adicionado func no submit para adicionar novos usuarios, o forms inicia inativo e aparece ao clicar em add, no html agora temos true e false em vez de 0 e 1

--- Fundamentos_front_end/Trabalho_Grupo/js/scriptClecio.js
+++ Fundamentos_front_end/Trabalho_Grupo/js/scriptClecio.js
@@ -1,1 +1,23 @@
 /* Script secundario criado para evitar conflitos nas codificações das funções */
+
+
+//Adicionar um funcionamento para enviar os dados do form para a tabela
+form.addEventListener('submit', (evento) => {
+    evento.preventDefault(); //Evita que a página seja recarregada
+    let usuario = { //Cria um objeto com os dados do form
+        id: tabela.tBodies[0].rows.length + 1,
+        nome_completo: form.nome.value,
+        nome_usuario: form.nomeUS.value,
+        senha: form.senha.value,
+        ativo: form.ativo.options[form.ativo.selectedIndex].label,
+        papel: form.papel.options[form.papel.selectedIndex].label
+    };
+    //console.log(usuario);
+    inserirProfissional(usuario); //insere o profissional na tabela
+    form.reset(); //Limpa os campos do form
+    form.classList.add('inativo'); //Esconde o form
+    eventoExcluir(); //Adiciona o evento de excluir ao botao criado ao inserir nova linha na tabela
+});
+
+
+// id, ativo, nome_completo, nome_usuario, papel, senha
