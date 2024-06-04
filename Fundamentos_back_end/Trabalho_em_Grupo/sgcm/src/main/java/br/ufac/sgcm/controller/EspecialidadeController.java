59da79e130b3fd0ebdf98c700a38141c227ad031
SantRocha <santiagomelo121@gmail.com>
2024-06-04 14:48:17 -0500

    arquivos

---
+++ Fundamentos_back_end/Trabalho_em_Grupo/sgcm/src/main/java/br/ufac/sgcm/controller/EspecialidadeController.java
@@ -0,0 +1,89 @@
+package br.ufac.sgcm.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.ufac.sgcm.dao.EspecialidadeDao;
+import br.ufac.sgcm.model.Especialidade;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class EspecialidadeController implements IController<Especialidade> {
+
+    private EspecialidadeDao eDao;
+
+    public EspecialidadeController() {
+        eDao = new EspecialidadeDao();
+    }
+
+    @Override
+    public int delete(Especialidade objeto) {
+        int registrosAfetados = eDao.delete(objeto);
+        return registrosAfetados;
+    }
+
+    @Override
+    public List<Especialidade> get() {
+        List<Especialidade> registros = eDao.get();
+        return registros;
+    }
+
+    @Override
+    public Especialidade get(Long id) {
+        Especialidade registro = eDao.get(id);
+        return registro;
+    }
+
+    @Override
+    public List<Especialidade> get(String termoBusca) {
+        List<Especialidade> registros = eDao.get(termoBusca);
+        return registros;
+    }
+
+    @Override
+    public int save(Especialidade objeto) {
+        int registrosAfetados = 0;
+        if (objeto.getId() == null)
+            registrosAfetados = eDao.insert(objeto);
+        else
+            registrosAfetados = eDao.update(objeto);
+        return registrosAfetados;
+    }
+
+    // Metodos do Servlet
+    public List<Especialidade> processListRequest(HttpServletRequest req) {
+        List<Especialidade> registros = new ArrayList<>();
+        String paramExcluir = req.getParameter("excluir");// excluir
+        if (paramExcluir != null && !paramExcluir.isEmpty()) {
+            Especialidade esp = new Especialidade();
+            Long id = Long.parseLong(paramExcluir);
+            esp = this.get(id);
+            this.delete(esp);
+        }
+        registros = this.get();
+        return registros;
+    }
+
+    public Especialidade processFormRequest(HttpServletRequest req, HttpServletResponse res) {
+        Especialidade item = new Especialidade();
+        String submit = req.getParameter("submit");
+        String paramId = req.getParameter("id");
+        if (paramId != null && !paramId.isEmpty()) {
+            Long id = Long.parseLong(paramId);
+            item.setId(id);
+            this.save(item);
+        }
+        if (submit != null) {
+            item.setNome(req.getParameter("nome"));
+            this.save(item);
+        }
+        try {
+            res.sendRedirect("Especialidades.jsp");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return item;
+    }
+
+}
---
+++ Fundamentos_back_end/Trabalho_em_Grupo/sgcm/src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>Hello World É o KARALHOOOOO!!!!!</h2>
+<h2><a href="especialidades.jsp">redirecionar</a></h2>
+</body>
+</html>
